175cca075adccd896184cd51b5783f951fe412cc
Iain Saxon <iain.saxon@newism.com.au>
2011-03-11 10:02:44 +1100

    Bumped version number

--- views/module/report/index.php
+++ views/module/report/index.php
@@ -5,7 +5,7 @@
  * This file is used when Nsm_reports_mcp::index() is called and returns a table of all reports found in reports directory to the user.
  *
  * @package NsmReports
- * @version 1.0.1
+ * @version 1.0.2
  * @author Leevi Graham <http://leevigraham.com.au>
  * @author Iain Saxon <iain.saxon@newism.com.au> * @copyright Copyright (c) 2007-2011 Newism <http://newism.com.au>
  * @license Commercial - please see LICENSE file included with this distribution
